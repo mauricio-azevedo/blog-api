8f862b533e0c7466afcce9c9dca2590461f7dfd9
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2024-07-19 17:24:23 -0300

    Add Devise for user authentication
    This update incorporates Devise into the user model, enabling features like password encryption, token generation for reset password, and remember me functionality. It also includes necessary configuration in Devise initializer and related migration file. The required routes have been added and default URL options for action mailer are configured.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_19_155228) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_19_202033) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_19_155228) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "posts"
