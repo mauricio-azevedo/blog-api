72ec7ce1bca9f730c60386b149802638fda38be2
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2024-07-20 12:57:34 -0300

    Remove Welcome module and simplify response rendering
    Deleted the welcome controller, views and routing as they prevailed redundant. Moved the response rendering path from 'shared/response' to just 'response'. The change reduces the complexity of path and eliminates the need for the shared directory.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,12 +8,12 @@ module Users
       if resource.persisted?
         @data = @user.slice(:id, :email, :name, :created_at, :updated_at)
         @message = 'Signed up successfully'
-        render 'shared/response', status: :ok
+        render 'response', status: :ok
       else
         @ok = false
         @message = 'Sign up failed'
         @details = resource.errors.full_messages
-        render 'shared/response', status: :unprocessable_entity
+        render 'response', status: :unprocessable_entity
       end
     end
   end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,11 +7,11 @@ module Users
         sign_in(@user)
         @data = @user.slice(:id, :email, :name, :created_at, :updated_at)
         @message = 'Signed in successfully.'
-        render 'shared/response', status: :ok
+        render 'response', status: :ok
       else
         @ok = false
         @message = 'Invalid Email or Password'
-        render 'shared/response', status: :unauthorized
+        render 'response', status: :unauthorized
       end
     end
 
@@ -26,11 +26,11 @@ module Users
     def respond_to_on_destroy
       if @user_signed_out
         @message = 'Signed out successfully.'
-        render 'shared/response', status: :ok
+        render 'response', status: :ok
       else
         @ok = false
         @message = 'No active session found'
-        render 'shared/response', status: :unprocessable_entity
+        render 'response', status: :unprocessable_entity
       end
     end
   end
