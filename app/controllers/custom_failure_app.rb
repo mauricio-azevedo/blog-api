04c483f0e58783bccc06ae3fc3bcc6f7f4924aa6
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2024-07-20 16:56:22 -0300

    Implement dynamic json error response rendering
    The update replaces the static JSON error response in the custom failure app with a dynamic error response. It extracts error rendering into a reusable private method. The change allows for better control over the error message content and format by using a shared template.

--- app/controllers/custom_failure_app.rb
+++ app/controllers/custom_failure_app.rb
@@ -10,6 +10,15 @@ class CustomFailureApp < Devise::FailureApp
   def json_error_response
     self.status = 401
     self.content_type = 'application/json'
-    self.response_body = { ok: false, data: nil, message: i18n_message }.to_json
+    self.response_body = render_to_string(template: 'shared/response', formats: [:json])
+  end
+
+  private
+
+  def render_to_string(template:, formats:)
+    controller = ActionController::Base.new
+    controller.instance_variable_set(:@ok, false)
+    controller.instance_variable_set(:@message, i18n_message)
+    controller.render_to_string(template: template, formats: formats)
   end
 end
