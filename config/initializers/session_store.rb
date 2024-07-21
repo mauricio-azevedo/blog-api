b0aa3e7e80dbdaec32f4493a9f28a6c5a0cc56ae
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2024-07-21 14:59:17 -0300

    Update session store configurations
    The configuration for the session store has been modified to allow for cross-domain cookies. The 'domain' attribute has been added and set to ':all' in order to apply to all subdomains, and the tld_length param was set to '2' to handle top-level domains with a single subdomain.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_blog_session', same_site: :none, secure: Rails.env.production?
+Rails.application.config.session_store :cookie_store, key: '_blog_session', domain: :all, tld_length: 2, same_site: :none, secure: Rails.env.production?
