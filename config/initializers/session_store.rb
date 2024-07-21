78fb8f33c5c2ed881412b25f72f61e3f86eb4c3a
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2024-07-21 14:30:56 -0300

    Update session key in session_store config
    This commit changes the key in the session_store configuration from '_session_id' to '_blog_session'. This is a minor update aimed at enhancing the uniqueness of our session identifiers, potentially improving session management efficiency.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_session_id', same_site: :none, secure: Rails.env.production?
+Rails.application.config.session_store :cookie_store, key: '_blog_session', same_site: :none, secure: Rails.env.production?
