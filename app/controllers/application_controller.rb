10a6455e9e9ca66ddc1e22abc7049dd27e1e90f8
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2024-07-22 09:43:53 -0300

    Add devise parameters configuration in application controller
    The `application_controller.rb` has been updated to include a configuration of permitted parameters for devise controlled resources. This update is essential for security reasons as it ensures only the allowed parameters are accepted.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
   respond_to :json
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, unless: :devise_controller?
 
   protected
